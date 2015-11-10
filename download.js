64b695c5b00a22af6c794a27ed51307b909259dd
Martin Bishop <martyn.bishop@gmail.com>
2015-11-10 17:47:45 +0200

    Update download.js

--- download.js
+++ download.js
@@ -3,16 +3,16 @@ var stream = require('stream');
 var util = require('util');
 
 // node v0.10+ use native Readable, else polyfill
-var Readable = stream.Readable ||
-    require('readable-stream').Readable;
-util.inherits(DownloadStream, Readable);
+var Writable = stream.Writable ||
+    require('readable-stream').Writable;
+util.inherits(DownloadStream, Writable);
 
 function DownloadStream(url, output) {
     // allow use without new
     if (!(this instanceof DownloadStream)) {
         return new DownloadStream(url, output);
     }
-    Readable.call(this, {});
+    Writable.call(this, {});
     this._output = output;
     var _self = this;
     http.get(url, function(res) {
@@ -28,7 +28,6 @@ function DownloadStream(url, output) {
 }
 DownloadStream.prototype._write = function (chunk, encoding, callback) {
     if(chunk.length == 0) {
-        this.push(null);
         this.emit('finish');
     }
     else {
